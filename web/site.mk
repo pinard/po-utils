73f68ea93164cfca931c8185f6898c3acf7fa7b0
François Pinard <pinard@iro.umontreal.ca>
2009-07-01 17:03:43 -0400

    Web site

---
+++ web/site.mk
@@ -0,0 +1,9 @@
+package_name = po-utils
+long_package_name = "Free PO utilities"
+rootdir = $(HOME)/fp/po-utils/web
+margin_color = "\#a3bdd7"
+caption_color = "\#dedeff"
+
+SITE_ROOT = 1
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
